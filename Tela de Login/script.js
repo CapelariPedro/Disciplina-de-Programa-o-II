ad3dbd9c3c062b1cedf5ab5a3be26f3f2d70d5b4
Pedro Vinicius de Morais Capelari <82915750+CapelariPedro@users.noreply.github.com>
2024-03-06 15:49:23 -0300

    Add files via upload

---
+++ Tela de Login/script.js
@@ -0,0 +1,16 @@
+const loginForm = document.getElementById("login-form");
+const loginBottun = document.getElementById("login-form-submit");
+const loginMsgError = document.getElementById("login-error-msg");
+
+loginBottun.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "aluno" && password === "exemplo@"){
+        alert("Você fez login com sucesso !!!");
+        location.reload();
+    } else {
+        loginMsgError.style.opacity = 1;
+    }
+});
